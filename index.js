cdc79ebee507d26133106ff0b886ed30a86ba424
褚林银 <chulinyin@bianfeng.com>
2018-10-17 13:36:30 +0800

    update test

---
+++ index.js
@@ -0,0 +1,53 @@
+$(function() {
+  var UA = window.navigator.appVersion;
+  var isIos = UA.match(/iphone/gi);
+  var isAndroid = UA.match(/android/gi);
+  var isWeChat = UA.match(/MicroMessenger/gi);
+
+  var UI = {
+    $wechat: $('#wechat'),
+    $web: $('#web'),
+    $android: $('#android'),
+    $ios: $('#ios'),
+    $wechatTipBrowser: $('.wechat_tip_browser'),
+    $iosDownloadBtn: $('#ios .download_btn'),
+    $iosDownloadedTip: $('#ios .downloaded_tip')
+  };
+
+  var Page = {
+    init: function () {
+      Page.listen()
+
+      if(isWeChat) {
+        Page.renderWeChat()
+      } else if(isIos) {
+        Page.renderIos()
+      } else if(isAndroid) {
+        Page.renderAndroid()
+      } else {
+        Page.renderWeb()
+      }
+    },
+    listen: function () {
+      UI.$iosDownloadBtn.on('click', function() {
+        UI.$iosDownloadBtn.hide()
+        UI.$iosDownloadedTip.show()
+      })
+    },
+    renderWeChat: function () {
+      UI.$wechat.show()
+      UI.$wechatTipBrowser.html(isIos ? 'Safari' : '浏览器')
+    },
+    renderIos: function () {
+      UI.$ios.show()
+    },
+    renderAndroid: function () {
+      UI.$android.show()
+    },
+    renderWeb: function () {
+      UI.$web.show()
+    },
+  };
+
+  Page.init();
+})
